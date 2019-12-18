6fde37d72c06d424dcfce94a893f64fbfd914ab6
jorritboer <jorritdb@hotmail.com>
2019-12-18 19:06:35 +0100

    Add outliar removal

--- project.py
+++ project.py
@@ -2,9 +2,20 @@ import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt
 
-data = pd.read_csv("train.csv") 
+data = np.array(pd.read_csv('train.csv'))
 
-plt.boxplot(data.trip_duration) # shows very very big outliars
-# print(data.id)
+# outliar removal
+treshold = 6 # maximum amount of hours considered a reasonable trip length
+filtered_data = data[ data[:,10] < (treshold * 60 * 60) ]
+
+# visualize outliar removal
+plt.subplot(211)
+plt.title('Trip lengths before outliar removal')
+plt.xlabel('Trip length in seconds')
+plt.boxplot(data[:,10], vert=False) # shows very very big outliars
+plt.subplot(212)
+plt.title('Trip lengths after trips above ' + str(treshold) + ' are removed')
+plt.xlabel('Trip length in seconds')
+plt.boxplot(filtered_data[:,10], vert=False) # outliars removed
 
 plt.show()
